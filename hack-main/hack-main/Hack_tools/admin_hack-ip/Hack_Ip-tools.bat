d6c17ff26b60397c6fe2c03f37d97f78f84493c2
RRAAPTcOMEBACK <155345423+RRAAPTcOMEBACK@users.noreply.github.com>
2024-01-08 07:00:06 -0800

    Update Hack_Ip-tools.bat

--- hack-main/hack-main/Hack_tools/admin_hack-ip/Hack_Ip-tools.bat
+++ hack-main/hack-main/Hack_tools/admin_hack-ip/Hack_Ip-tools.bat
@@ -1,4 +1,11 @@
 @echo off
+
+IF NOT "%~1"=="START_HACK.exe" (
+  echo This batch file is for internal use only.
+  echo If you are not a developer, please contact your administrator.
+  exit /b
+)
+
 color c
 SET /p webhook=" webhook >  "
 cd Hack_target
